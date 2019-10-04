3c2ec785221936bb12b967bd94e00eca9a8d34d1
Péter Bozsó <peter.bozso@outlook.com>
2019-10-04 16:48:06 +0300

    Add controller to test status codes

---
+++ src/HttpCat.Api/Controllers/StatusCodeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpCat.Api.Controllers
+{
+    [ApiController]
+    public class StatusCodeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{statusCode:int:required}")]
+        public StatusCodeResult Get(int statusCode)
+        {
+            return StatusCode(statusCode);
+        }
+    }
+}
